469241147f964d2ef62b49c4d3b99c5661dbd8a7
Nguyen Trieu <danneytrieu@gmail.com>
2022-01-04 12:38:46 +0700

    successfuly get data and displayed in card's detail

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-//
+// FUNCTION: Hide/Show buttons
+const settingBtn = document.querySelector("#setting-btn");
+const buttons = document.querySelectorAll(".nav .button");
+// Toggle show classlist to all setting's buttons
+buttons.forEach((button) => {
+  settingBtn.addEventListener("click", (e) => {
+    button.classList.toggle("show");
+  });
+});
 //
 //
 //
@@ -6,16 +14,19 @@
 //
 // FUNCTION: API and display data on html
 import FetchWrapper from "./fetch-wrapper.js";
-const key = "?apiKey=93d3b9134b1d4c44ae5f9dd1b9800b0d" + "&";
-//main key 93d3b9134b1d4c44ae5f9dd1b9800b0d danneytrieu
-//backup key 9cb20095529e4d41a996938e730404a6 danneytrieuwork
+const key = "?apiKey=9cb20095529e4d41a996938e730404a6" + "&";
+//main key ?apiKey=93d3b9134b1d4c44ae5f9dd1b9800b0d danneytrieu
+//backup key ?apiKey=9cb20095529e4d41a996938e730404a6 danneytrieuwork
+//backup key ?apiKey=a41414bac2184fb09f45f9c7fd1a3fc6 jlim
+//backup key ?apiKey=5129b66ccd2e4b24bbf9a50c64043303 phanuyenryna
 const API = new FetchWrapper("https://api.spoonacular.com/");
 const generateMeals = async () => {
   const datas = await API.get(
     //endpoint details: https://spoonacular.com/food-api/docs#Search-Recipes-by-Nutrients
     //this endpoint allow to collect datas base on macro nutrients value
-    `recipes/findByNutrients${key}?maxCalories=500&minCarbs=20&minProtein=20&minFat=20&number=31`
+    `recipes/findByNutrients${key}?maxCalories=300&minCarbs=15&minProtein=15&minFat=15&number=31`
   );
+  console.log(datas);
   let card = "";
   //inject user's info: id (use to retrieve detail), image, title, calories, carbs, protein, fat
   datas.map((data) => {
@@ -27,9 +38,7 @@ const generateMeals = async () => {
               <i id="trash" class="far fa-trash-alt"></i>
             </div>
             <div class="img-wrapper">
-              <button draggable="false" class="item-info">
-                <a draggable="false" href="#">VIEW</a>
-              </button>
+              <button draggable="false" class="item-info">VIEW</button>
               <img
                 class="item-img"
                 src="${data.image}"
@@ -39,7 +48,7 @@ const generateMeals = async () => {
             </div>
             <article class="content-front">
               <h3 class="item-title">${data.title.substring(0, 20)}</h3>
-              <p class="item-calories">(${data.calories} Cal)</p>
+              <p class="item-calories">(${data.calories} Kcal)</p>
             </article>
             <article class="content-back">
               <p class="item-protein">Protein: ${data.protein}</p>
@@ -69,7 +78,6 @@ const generateMeals = async () => {
   //
   //
   //
-
   // FUNCTION: Dragable function
   const listItems = document.querySelectorAll(".list-item");
   const lists = document.querySelectorAll("#list");
@@ -103,43 +111,66 @@ const generateMeals = async () => {
     // Fire event for enter
     list.addEventListener("dragenter", function (e) {
       e.preventDefault();
-      this.style.background = "var(--lightblue)";
-      this.style.border = "2px dotted var(--darkgrey)";
-      this.style.borderRadius = "5px";
+      e.currentTarget.style.background = "var(--lightblue)";
+      e.currentTarget.style.border = "2px dotted var(--darkgrey)";
+      e.currentTarget.style.borderRadius = "5px";
     });
     // Fire event for leave
     list.addEventListener("dragleave", function (e) {
-      this.style.border = "";
-      this.style.background = "";
+      e.currentTarget.style.border = "";
+      e.currentTarget.style.background = "";
     });
     // Fire event for drop
     list.addEventListener("drop", function (e) {
-      this.append(draggedItem);
-      this.style.border = "";
-      this.style.background = "";
+      e.currentTarget.append(draggedItem);
+      e.currentTarget.style.border = "";
+      e.currentTarget.style.background = "";
     });
   });
   //
-};
-// TODO: generateMeals();
-//
-//
-//
-//
-//
-//
-//
-// FUNCTION: Hide/Show buttons
-const settingBtn = document.querySelector("#setting-btn");
-const buttons = document.querySelectorAll(".nav .button");
-// Toggle show classlist to all setting's buttons
-buttons.forEach((button) => {
-  settingBtn.addEventListener("click", (e) => {
-    button.classList.toggle("show");
-  });
-});
+  //
+  //
+  //
+  //
+  // FUNCTION: MODAL
+  const modalOpenBtns = document.querySelectorAll(".item-info");
+  const modalCloseBtn = document.querySelector(".modal-close");
+  const modalContainer = document.querySelector(".modal-container");
+  const modalContent = document.querySelector(".modal-content");
 
+  // hide-show modal
+  modalOpenBtns.forEach((btn) => {
+    // handle hide modal
+    btn.addEventListener("click", async (e) => {
+      modalContainer.classList.add("show-hide");
+      // receive mealID to use for recipe detail API
+      const mealID =
+        e.currentTarget.parentElement.parentElement.lastElementChild.innerHTML;
+      const data = await API.get(
+        `recipes/${mealID}/information${key}includeNutrition=true`
+      );
+      // insert content
+      modalContent.innerHTML = `
+          <h3 class="modal-title">${data.title}</h3>
+          <h4 class="modal-type">Dish Type: ${data.dishTypes}</h4>
+          <h5 class="modal-servings">Servings Size: ${data.servings}</h5>
+          <p class="modal-summary">Summary: ${data.summary}</p>
+          `;
+    });
+    //close modal
+    modalCloseBtn.addEventListener("click", (e) => {
+      modalContainer.classList.remove("show-hide");
+    });
+  });
+};
+TODO: generateMeals();
 //
+//
+//
+//
+//
+//
+
 //
 //
 //
